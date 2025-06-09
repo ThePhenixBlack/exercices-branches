32e3e0938b3916f7f0918fe81c223ca54167458b
matteo <matteo@MacBook-Air-de-matteo.local>
2025-06-09 16:56:34 +0200

    exercice suite

---
+++ fonctions/exercice2.js
@@ -0,0 +1,15 @@
+let x = prompt("Entrée un numbre") ;
+
+function carre(number1)
+{
+    return Math.pow(number1,2)
+}
+console.log(carre(x))
+
+let result = carre(x)
+
+function multiplyBy10(number)
+{
+    return number * 10
+}
+console.log(multiplyBy10(result))
